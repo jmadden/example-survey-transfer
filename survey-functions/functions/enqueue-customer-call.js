cd3c3efcfe090be41ef9e684341d80758bf5086f
Jim Madden <jmadden@twilio.com>
2021-07-14 06:56:42 -0700

    removed logging

--- survey-functions/functions/enqueue-customer-call.js
+++ survey-functions/functions/enqueue-customer-call.js
@@ -4,11 +4,7 @@ let path = Runtime.getFunctions()['survey-utils'].path;
 let assets = require(path);
 
 exports.handler = TokenValidator(async (context, event, callback) => {
-  console.log('CALL SID: ', event.callSid);
-  // let sample = { callerStatus: 'enqueued', message: 'survey connected' };
-  // let body = new Promise(function (resolve, reject) {
-  //   setTimeout(() => resolve(sample), 1000);
-  // });
+  console.log('CALL SID TO ENQUEUE: ', event.callSid);
 
   const client = context.getTwilioClient();
 
